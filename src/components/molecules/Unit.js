e787481a78079e7563b7f117fb0a869f7efddee1
rmkubik <rmkubik@me.com>
2017-11-18 15:43:23 -0800

    Remove unsused stateful logic

--- src/components/molecules/Unit.js
+++ src/components/molecules/Unit.js
@@ -2,28 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export class Unit extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            unitClassName: ""
-        };
-    }
-
-    // componentWillLeave() {
-    //     this.setState({unitClassName: " animated bounce"});
-    // }
-
     render() {
         return (
                 <span
-                    className={"unit" + this.state.unitClassName}
+                    className={"unit"}
                 >
                     {this.props.unit ? this.props.unit.icon : ""}
                 </span>
         )
     }
-}
-
-Unit.propTypes = {
-    unit: PropTypes.object
 }
